4f74561daa1bf27de91e34902d23076c52776a73
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBAM.ORG.BR>
2018-10-08 18:23:20 -0400

    implementacao de autenticacao

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -11,7 +11,7 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {SecurityConfiguration.class};
+		return new Class[] {SecurityConfiguration.class,AppWebConfiguration.class, JPAConfiguration.class};
 	}
 
 	@Override
